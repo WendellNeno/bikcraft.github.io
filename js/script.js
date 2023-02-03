1ca805c6addfc20be55782da92c39f55fcebf583
wendellneno <wendelleduardo09@gmail.com>
2023-02-02 21:59:09 -0300

    galeria-imagens

--- js/script.js
+++ js/script.js
@@ -25,14 +25,12 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 // perguntas frquentes
-
 const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
-
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
   console.log(ativa);
@@ -44,3 +42,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 920px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
